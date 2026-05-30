d2bea3f0227a866e5fcf2ffde7042e527ed0ff4c
Ruslan <ruslan.minatullaev-1000@yandex.ru>
2026-05-30 22:55:55 +0300

    feat: Remove extra files

--- dev/js/critical.js
+++
@@ -1,5 +0,0 @@
-// import "./effects/customCursor.js";
-// import "./effects/gridBackground.js"; 
-// import "./animations/heroAnimations.js"; 
-// import "./animations/parallaxOnScroll.js";
-// import "./animations/scrollReveal.js";
